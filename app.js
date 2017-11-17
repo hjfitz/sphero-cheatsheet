fa096670b45167473fc50065b2c28821880c0e5e
Harry <fitzgerald_harry@network.lilly.com>
2017-11-17 10:42:25 +0000

    move to gs pages friendly

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const logger = require('morgan')('dev');
 const compression = require('compression')();
 
 const app = express();
-const public = path.join(__dirname, 'public');
-const index = path.join(public, 'index.html');
+const public = path.join(__dirname, 'docs');
 
 app.use(logger);
 app.use(compression);
