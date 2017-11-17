7f12418f3a6f8be1423f4443027eeece0226164a
Harry <fitzgerald_harry@network.lilly.com>
2017-11-17 15:40:02 +0000

    smooth scrolling

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,19 @@
-var elem = document.querySelector('.sidenav');
-var instance = new M.Sidenav(elem);
+// fetch badboys from DOM
+const items = document.querySelectorAll('.menu-item');
+const sideNav = document.querySelector('.sidenav');
+
+// instantiate a mobile side-nav
+const instance = new M.Sidenav(sideNav);
+
+// use a cheeky bit of jq to scroll nicely
+// for those who don't support elem#scrollintoview({ smooth })
+const jump = ({ target }) => {
+  const { jump: sel } = target.dataset;
+  const offset = $(sel).offset().top - 80;
+  $('html, body').animate({ scrollTop: offset}, 750);
+}
+
+// add click listeners
+items.forEach(item => {
+  item.addEventListener('click', jump);
+});
